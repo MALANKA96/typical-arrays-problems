b80c574b07fa535668aaf12a77e870ba2bdd267b
malanka96 <voksa.kolya@gmail.com>
2020-10-03 23:18:10 +0300

    feat: add index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
+exports.min = function min(array) {
+    if (Array.isArray(array) && array.length != 0) {
+        let min_el = array[0];
+        for (var i = 0; i < array.length; i++) {
+            if (min_el > array[i]) {
+                min_el = array[i];
+            }
+        }
+        return min_el;
+    }
+    return 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (Array.isArray(array) && array.length != 0) {
+        let max_el = array[0];
+        for (var i = 0; i < array.length; i++) {
+            if (max_el < array[i]) {
+                max_el = array[i];
+            }
+        }
+        return max_el;
+    }
+    return 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (Array.isArray(array) && array.length != 0) {
+        let sum = 0;
+        for (var i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum / array.length;
+    } else {
+        return 0;
+    }
+};
